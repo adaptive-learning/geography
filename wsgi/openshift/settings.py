f2d45633f43a9f780cd093907d1a9083731fb835
Vít Stanislav <slaweet@seznam.cz>
2013-06-26 07:42:11 +0200

    testing

--- wsgi/openshift/settings.py
+++ wsgi/openshift/settings.py
@@ -156,7 +156,6 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'core',
-    'south',
 )
 
 # A sample logging configuration. The only tangible logging
