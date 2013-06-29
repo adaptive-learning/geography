870f74e8095f482015aa2b5b08d96805c2a41814
Vít Stanislav <slaweet@seznam.cz>
2013-06-29 13:39:26 +0200

    add csrf token to homepage

--- wsgi/openshift/views.py
+++ wsgi/openshift/views.py
@@ -1,6 +1,9 @@
 import os
 from django.shortcuts import render_to_response
+from django.core.context_processors import csrf
 
 def home(request):
     request.META["CSRF_COOKIE_USED"] = True
-    return render_to_response('home/home.html')
+    c = {}
+    c.update(csrf(request))
+    return render_to_response('home/home.html', c)
