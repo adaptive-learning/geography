3067693e2200ab1cd1bad2d638261211c6d785e5
Vít Stanislav <slaweet@seznam.cz>
2013-06-29 09:06:09 +0200

    still not working

--- setup.py
+++ setup.py
@@ -1,6 +1,18 @@
 #!/usr/bin/env python
 
 from setuptools import setup
+import os
+import sys
+ 
+os.environ['DJANGO_SETTINGS_MODULE'] = 'openshift.settings'
+sys.path.append(os.path.join(os.environ['OPENSHIFT_REPO_DIR'], 'wsgi', 'openshift'))
+virtenv = os.environ['APPDIR'] + '/virtenv/'
+os.environ['PYTHON_EGG_CACHE'] = os.path.join(virtenv, 'lib/python2.6/site-packages')
+virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
+try:
+    execfile(virtualenv, dict(__file__=virtualenv))
+except:
+    pass
 
 setup(
     name='YourAppName',
@@ -9,5 +21,5 @@ setup(
     author='Your Name',
     author_email='example@example.com',
     url='http://www.python.org/sigs/distutils-sig/',
-    install_requires=['Django<=1.4'],
+    install_requires=['Django<=1.4', 'south', 'lazysignup'],
 )
