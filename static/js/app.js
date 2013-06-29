a55fd4e0b58c200461b9edc83ae9d9c95ea85e33
Vít Stanislav <slaweet@seznam.cz>
2013-06-29 13:50:27 +0200

    static folder link reverted

---
+++ static/js/app.js
@@ -0,0 +1,22 @@
+'use strict';
+
+
+// Declare app level module which depends on filters, and services
+angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives', 'myApp.controllers', 'ngCookies'])
+
+  .config(function($routeProvider) {
+    $routeProvider.when('/', {
+      redirectTo: '/view/USA'
+    }).when('/view/:part', {
+      controller : 'AppView',
+      templateUrl : './static/tpl/view_tpl.html'
+    }).when('/practice/:part', {
+      controller : 'AppPractice',
+      templateUrl : './static/tpl/practice_tpl.html'
+    }).when('/practice/:part/:type', {
+      controller : 'AppPractice',
+      templateUrl : './static/tpl/practice_tpl.html'
+    }).otherwise({
+      //redirectTo: '/'
+    });
+  })
