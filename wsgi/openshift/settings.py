883a6d3cbbd535fb247665dca8a1ee2e154a7876
Vit Stanislav <slaweet@mail.muni.cz>
2013-09-01 13:32:41 +0200

    south added

--- wsgi/openshift/settings.py
+++ wsgi/openshift/settings.py
@@ -169,7 +169,7 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
-#     'south',
+    'south',
     'social_auth',
     'lazysignup',
     'core',
