e1555cfff700a7c812b2fc465f718ad4323ca830
Vit Stanislav <slaweet@mail.muni.cz>
2013-10-27 11:22:45 +0100

    Bugfix in update_confused_places command

--- wsgi/openshift/core/management/commands/update_confused_places.py
+++ wsgi/openshift/core/management/commands/update_confused_places.py
@@ -3,7 +3,6 @@ from core.models import PlaceRelation
 from django.core.management.base import NoArgsCommand
 from django.db.models import F
 from questions.models import Answer, ConfusedPlaces
-from questions.utils import FindOnMapQuestionType
 
 
 class Command(NoArgsCommand):
@@ -16,5 +15,5 @@ class Command(NoArgsCommand):
             for place2 in PlaceRelation.objects.get_bordering_places(pr.place):
                 ConfusedPlaces.objects.was_confused(pr.place, place2)
 
-        for a in Answer.objects.exclude(place=F("answer")).filter(type=FindOnMapQuestionType().id):
+        for a in Answer.objects.exclude(place=F("answer")).filter(type=Answer.FIND_ON_MAP):
             ConfusedPlaces.objects.was_confused(a.place, a.answer)
