72b4bb8529b8cbd51da037dff8ddabc1cfb7098c
Jan Papoušek <jan.papousek@gmail.com>
2014-01-25 18:16:46 +0100

    do not store absolute path in export archive

--- .openshift/cron/daily/data_export.sh
+++ .openshift/cron/daily/data_export.sh
@@ -4,5 +4,5 @@
 source $OPENSHIFT_HOMEDIR/python/virtenv/bin/activate
 
 python "$OPENSHIFT_REPO_DIR"wsgi/openshift/manage.py csv_answers ${OPENSHIFT_DATA_DIR}export.csv
-zip ${OPENSHIFT_DATA_DIR}export_csv ${OPENSHIFT_DATA_DIR}export.csv
+zip --junk-paths ${OPENSHIFT_DATA_DIR}export_csv ${OPENSHIFT_DATA_DIR}export.csv
 rm -rf ${OPENSHIFT_DATA_DIR}export.csv
