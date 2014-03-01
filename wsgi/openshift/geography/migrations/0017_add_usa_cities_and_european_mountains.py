d27b2b9f7e0641592aba1835843159cf3a88288c
Vit Stanislav <slaweet@seznam.cz>
2014-03-01 14:34:39 +0100

    Add U.S. cities and European mountains properly

--- wsgi/openshift/geography/migrations/0017_add_usa_cities_and_european_mountains.py
+++ wsgi/openshift/geography/migrations/0017_add_usa_cities_and_european_mountains.py
@@ -1,40 +1,45 @@
 # -*- coding: utf-8 -*-
 from south.v2 import DataMigration
+from django.core.management import call_command
 from django.db import connection
 
 
 class Migration(DataMigration):
+
+    dependencies = [("migrations", "0016_answers_on_map")]
+
     TRANSLATE = {
         u'Hardanger-vidda': u'Hardangervidda',
         u'Böhmerwald': u'Šumava',
         u'Transylvanian Alps': u'Jižní Karpaty',
-        u'Tatra Mts.': u'Tatry',
-        u'Pindus Mts.': u'Pindos',
+        u'Tatra_Mts.': u'Tatry',
+        u'Pindus_Mts.': u'Pindos',
         u'Calabria': u'Kalábrie',
         u'Harz': u'Harz',
         u'Vosges': u'Vogézy',
-        u'Sudetes Mts.': u'Sudetská pohoří',
+        u'Sudetes_Mts.': u'Sudetská pohoří',
         u'Erzgebirge': u'Krušné hory',
         u'Ardennes': u'Ardeny',
         u'Jura': u'Jura',
-        u'Grampian Mts.': u'Grampiany',
-        u'Cambrian Mts.': u' Kumbrické hory',
-        u'Balkan Mts.': u'Stará planina',
-        u'Dinaric Alps': u'Dinárské hory',
-        u'Cord. Cantábrica': u'Kantaberské pohoří',
-        u'S. Nevada': u'Sierra Nevada',
-        u'Sierra Morena': u'Sierra Morena',
-        u'KJØLEN MOUNTAINS': u'Skandinávské pohoří',
-        u'CARPATHIAN MOUNTAINS': u'Karpaty',
+        u'Grampian_Mts.': u'Grampiany',
+        u'Cambrian_Mts.': u' Kumbrické hory',
+        u'Balkan_Mts.': u'Stará planina',
+        u'Dinaric_Alps': u'Dinárské hory',
+        u'Cord._Cantábrica': u'Kantaberské pohoří',
+        u'S._Nevada': u'Sierra Nevada',
+        u'Sierra_Morena': u'Sierra Morena',
+        u'KJLEN_MOUNTAINS': u'Skandinávské pohoří',
+        u'CARPATHIAN_MOUNTAINS': u'Karpaty',
         u'APPENNINI': u'Apeniny',
         u'PYRENEES': u'Pyreneje',
         u'ALPS': u'Alpy',
-        u'CAUCASUS MTS.': u'Kavkaz',
+        u'CAUCASUS_MTS.': u'Kavkaz',
     }
 
-    dependencies = [("migrations", "0016_answers_on_map")]
-
     def forwards(self, orm):
+        call_command('add_map', 'europe', u'Evropa', 'MOUNTAINS')
+        call_command('add_map', 'us', u'USA', 'CITY')
+
         cursor = connection.cursor()
         for code, name in Migration.TRANSLATE.iteritems():
             cursor.execute(
