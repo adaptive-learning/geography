392904fc66302c5db9192c9466a0e33b9e407b9b
Vit Stanislav <slaweet@seznam.cz>
2014-03-15 17:27:18 +0100

    Update places in DB with places in svg files on deploy

--- wsgi/openshift/geography/management/commands/update_maps.py
+++ wsgi/openshift/geography/management/commands/update_maps.py
@@ -1,5 +1,7 @@
 from django.core.management.base import BaseCommand
 from geography.management import MapUpdater
+from django.db import connection
+import csv
 
 
 class Command(BaseCommand):
@@ -8,3 +10,30 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         u = MapUpdater()
         u.update_all_maps()
+        self.translate()
+
+    def translate(self):
+        translations = self.get_translations()
+        cursor = connection.cursor()
+        print "Applying translations"
+        for code, name in translations:
+            cursor.execute(
+                '''
+                UPDATE
+                    geography_place
+                SET
+                    name = %s
+                WHERE code = %s OR name = %s
+                ''',
+                [name, code, code]
+            )
+        print "Done"
+
+    def get_translations(self):
+        translations = []
+        with open('translations.csv', 'rb') as csvfile:
+            translations_reader = csv.reader(csvfile, delimiter='\t')
+            for row in translations_reader:
+                if len(row) == 2:
+                    translations.append(row)
+        return translations
