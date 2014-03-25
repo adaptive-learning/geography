151d3d4ed2c5dc4f52c09b73b6a80ecd0dd939cf
Jan Papousek <jan.papousek@gmail.com>
2014-03-25 21:05:03 +0100

    skip some steps if there is no new commit

--- util/deploy.sh
+++ util/deploy.sh
@@ -61,23 +61,25 @@ echo " * new HEAD: $NEW_HEAD"
 # reset the application
 ###############################################################################
 
-echo " * collect static"
-$APP_DIR/manage.py collectstatic --noinput
-echo "HASHES = $( python $APP_DIR/manage.py static_hashes )" > $APP_DIR/hashes.py
+if [[ `$GIT_COMMAND diff --name-only $LAST_HEAD $DEPLOY_VERSION` ]]; then
+	echo " * collect static"
+	$APP_DIR/manage.py collectstatic --noinput
+	echo "HASHES = $( python $APP_DIR/manage.py static_hashes )" > $APP_DIR/hashes.py
 
-echo " * update maps"
-$APP_DIR/manage.py update_maps
+	echo " * update maps"
+	$APP_DIR/manage.py update_maps
 
-echo " * migrate"
-$APP_DIR/manage.py migrate geography --delete-ghost-migrations --traceback
-echo " * load custom SQLs"
-$APP_DIR/manage.py sqlcustom geography | $APP_DIR/manage.py dbshell
-if $GIT_COMMAND diff --name-only $LAST_HEAD $DEPLOY_VERSION | egrep 'main\/geography\/models\/(knowledge\.py|prior.py|current.py)'; then
+	echo " * migrate"
+	$APP_DIR/manage.py migrate geography --delete-ghost-migrations --traceback
+	echo " * load custom SQLs"
+	$APP_DIR/manage.py sqlcustom geography | $APP_DIR/manage.py dbshell
+	if [[ `$GIT_COMMAND diff --name-only $LAST_HEAD $DEPLOY_VERSION | egrep 'main\/geography\/models\/(knowledge\.py|prior.py|current.py)'` ]]; then
 	echo " * derive knowledge data"
 	$APP_DIR/manage.py derived_knowledge_data
+	fi
+	echo " * remove django cache"
+	rm -rf $DATA_DIR/.django_cache
 fi
-echo " * remove django cache"
-rm -rf $DATA_DIR/.django_cache
 
 
 ###############################################################################
@@ -90,5 +92,7 @@ if [ $GEOGRAPHY_ON_PRODUCTION ]; then
 	a2ensite production.slepemapy.cz
 fi
 
-echo " * reload httpd"
-service apache2 reload
+if [[ `$GIT_COMMAND diff --name-only $LAST_HEAD $DEPLOY_VERSION` ]]; then
+	echo " * reload httpd"
+	service apache2 reload
+fi
