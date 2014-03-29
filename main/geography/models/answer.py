9fb81bbcddfab490df350b217499a7b7fd2bb1f5
Jan Papousek <jan.papousek@gmail.com>
2014-03-29 21:45:35 +0100

    adjust target probability according to the previous success rate
    issues: #27

--- main/geography/models/answer.py
+++ main/geography/models/answer.py
@@ -14,7 +14,10 @@ class AnswerManager(models.Manager):
         answers = self.filter(
             user=user,
         ).order_by("-inserted")[:n]
-        return sum([a.place_asked_id == a.place_answered_id for a in answers]) / float(n)
+        if n:
+            return sum([a.place_asked_id == a.place_answered_id for a in answers]) / float(n)
+        else:
+            return 1.0
 
 
 class Answer(models.Model):
