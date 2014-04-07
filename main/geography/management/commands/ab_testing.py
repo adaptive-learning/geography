34ea415f2c5b3e64c7a62e7852a43c4bc3e1928b
Jan Papousek <jan.papousek@gmail.com>
2014-04-07 16:07:38 +0200

    exporting answers with values for the given A/B testing group
    issues: #27

--- main/geography/management/commands/ab_testing.py
+++ main/geography/management/commands/ab_testing.py
@@ -3,6 +3,8 @@ from django.core.management.base import BaseCommand, CommandError
 from geography.models import Group
 from optparse import make_option
 from prettytable import PrettyTable
+from django.db import connection
+from geography.utils.db import dump_cursor
 
 
 class Command(BaseCommand):
@@ -39,6 +41,7 @@ class Command(BaseCommand):
     the following subcommands are available
         * disable
         * enable
+        * export-answers
         * init
         * overview
         * stats-user
@@ -59,6 +62,8 @@ class Command(BaseCommand):
             return self.enable_disable(command_args, options, False)
         elif command == 'enable':
             return self.enable_disable(command_args, options, True)
+        elif command == 'export-answers':
+            return self.export_answers(command_args, options)
         elif command == 'init':
             return self.init(command_args, options)
         elif command == 'overview':
@@ -78,6 +83,41 @@ class Command(BaseCommand):
         group = Group.objects.get(name=group_name)
         group.active = active
         group.save()
+
+    def export_answers(self, args, options):
+        if options.get('command_help', False):
+            print self.help_export_answers()
+            return
+        group_name = args[0]
+        dest_file = args[1]
+        print 'exporting answers with group ' + group_name + ' to file ' + dest_file
+        cursor = connection.cursor()
+        cursor.execute(
+            '''
+            SELECT
+                geography_answer.*,
+                geography_ab_value.value AS ab_value
+            FROM
+                geography_answer
+                LEFT JOIN geography_answer_ab_values ON
+                    geography_answer_ab_values.answer_id = geography_answer.id
+                LEFT JOIN geography_ab_value ON
+                    geography_ab_value.id = geography_answer_ab_values.value_id
+                LEFT JOIN geography_ab_group ON
+                    geography_ab_group.id = geography_ab_value.group_id
+            WHERE
+                geography_ab_group.name = %s OR ISNULL(geography_ab_group.name)
+            GROUP BY
+                geography_answer.id
+            ''', [group_name])
+        field_mapping = {
+            'answer_id': 'answer',
+            'place_asked_id': 'place_asked',
+            'place_answered_id': 'place_answered'}
+        dump_cursor(
+            cursor,
+            dest_file,
+            **field_mapping)
 
     def init(self, args, options):
         if options.get('command_help', False):
@@ -134,11 +174,19 @@ class Command(BaseCommand):
         print table.get_string(sort_by='Value')
 
     def help_enable_disable(self):
-            return '''
-        enable/disable the given group
+        return '''
+    enable/disable the given group
 
-            ./manage.py ab_testing [enable|disable] <group name>
-                    '''
+        ./manage.py ab_testing [enable|disable] <group name>
+                '''
+
+    def help_export_answers(self):
+        return '''
+    exports answers together with the value for the given A/B testing group
+
+        ./manage.py ab_testing export-answers <group name> <dest file>
+                '''
+
 
     def help_init(self):
         return '''
@@ -155,8 +203,8 @@ class Command(BaseCommand):
                 '''
 
     def help_stats_answer(self):
-            return '''
-        show number of answers for the values of the given group
+        return '''
+    show number of answers for the values of the given group
 
-            ./manage.py ab_testing stats-answer <group name>
-                    '''
+        ./manage.py ab_testing stats-answer <group name>
+                '''
