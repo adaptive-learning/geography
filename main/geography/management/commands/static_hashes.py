4a54e1ca9d2ce42288f6a36b943c90313af2f706
Vit Stanislav <slaweet@seznam.cz>
2014-04-29 19:46:37 +0200

    static_hashes command: allow filtering input files

--- main/geography/management/commands/static_hashes.py
+++ main/geography/management/commands/static_hashes.py
@@ -3,30 +3,36 @@ import hashlib
 import json
 import os
 from django.conf import settings
+from re import search
 
 
 class Command(NoArgsCommand):
     help = u"""Compute hashes of static content files. Typically on deploy"""
 
-    def handle_noargs(self, **options):
-        self.stdout.write(json.dumps(self.get_hashes()))
+    def handle(self, *args, **options):
+        filter_ = ''
+        if len(args) > 0:
+            filter_ = args[0]
+        self.stdout.write(json.dumps(self.get_hashes(filter_)))
 
-    def get_hashes(self):
+    def get_hashes(self, filter_):
         hashes = {}
         module = "geography"
-        static_files = self.get_static_files(module)
+        static_files = self.get_static_files(module, filter_)
         for f in static_files:
             hashes[f[0]] = hashlib.md5(f[1]).hexdigest()
         return hashes
 
-    def get_static_files(self, module):
+    def get_static_files(self, module, filter_):
         files = []
         root = str(os.path.join(settings.PROJECT_DIR, module, 'static'))
         for path, subdirs, filenames in os.walk(root):
             for filename in filenames:
                 f = os.path.join(path, filename)
                 f = f.replace(root, 'static')
-                files.append(str(f))
+                f = str(f)
+                if search(filter_, f):
+                    files.append(f)
         return [(p, self.get_static_file_content(p, module)) for p in files]
 
     def get_static_file_content(self, filename, module):
