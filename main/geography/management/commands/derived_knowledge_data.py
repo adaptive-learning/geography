1a917aa206bfb28933818961ca973428fe7adf72
Jan Papousek <jan.papousek@gmail.com>
2014-05-15 09:30:37 +0200

    fix time measure

--- main/geography/management/commands/derived_knowledge_data.py
+++ main/geography/management/commands/derived_knowledge_data.py
@@ -85,6 +85,5 @@ class Command(BaseCommand):
         # save new precomputed datasets
         sys.stderr.write('flushing knowledge data to database\n')
         print env.flush()
-        time_after_flush = time.time()
-        sys.stderr.write('time: ' +  str(time.time() - time_after_flush) + ' secs\n')
+        sys.stderr.write('time: ' +  str(time.time() - time_after_delete) + ' secs\n')
         sys.stderr.write('total time: ' + str(time.time() - time_start) + ' secs\n')
