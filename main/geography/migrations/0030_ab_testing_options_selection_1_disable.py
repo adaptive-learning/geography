0063b45087961940545ae8eab69e12c61a32f6ec
Jan Papousek <jan.papousek@gmail.com>
2014-06-18 15:28:50 +0200

    disable options_selection_1 A/B experiment

---
+++ main/geography/migrations/0030_ab_testing_options_selection_1_disable.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from south.v2 import DataMigration
+from django.core.management import call_command
+from geography.models import Place
+
+class Migration(DataMigration):
+
+    dependencies = [("migrations", "0029_ab_testing_question_selection_1_disable")]
+
+
+    def forwards(self, orm):
+        call_command(
+            'ab_testing',
+            'disable',
+            'options_selection_1')
+
+    def backwards(self, orm):
+        call_command(
+            'ab_testing',
+            'enable',
+            'options_selection_1')
+
+    complete_apps = ['geography']
