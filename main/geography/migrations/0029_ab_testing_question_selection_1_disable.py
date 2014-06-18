70de3e8305b833822d95549513b6cb03b9a2caba
Jan Papousek <jan.papousek@gmail.com>
2014-06-18 15:28:24 +0200

    disable question_selection_1 A/B experiment

---
+++ main/geography/migrations/0029_ab_testing_question_selection_1_disable.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from south.v2 import DataMigration
+from django.core.management import call_command
+from geography.models import Place
+
+class Migration(DataMigration):
+
+    dependencies = [("migrations", "0028_myisam2innodb_test")]
+
+
+    def forwards(self, orm):
+        call_command(
+            'ab_testing',
+            'disable',
+            'question_selection_1')
+
+    def backwards(self, orm):
+        call_command(
+            'ab_testing',
+            'enable',
+            'question_selection_1')
+
+    complete_apps = ['geography']
