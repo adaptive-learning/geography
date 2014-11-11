423923b95f5a2597a4b9df5ad0c4331e89da1fb9
Jan Papousek <jan.papousek@gmail.com>
2014-11-11 16:53:58 +0100

    do not fetch all database in one time during data export

--- main/geography/management/commands/table2csv.py
+++ main/geography/management/commands/table2csv.py
@@ -2,21 +2,27 @@
 from django.core.management.base import BaseCommand, CommandError
 from django.db import connection
 from geography.utils.db import dump_cursor
+from contextlib import closing
 
 
 class Command(BaseCommand):
     help = 'dump model to csv file'
 
+    DEFAULT_BATCH_SIZE = 500000
+
     def handle(self, *args, **options):
-        if len(args) != 2:
+        if len(args) != 2 and len(args) != 3:
             raise CommandError(
                 '''
                 The command requires exactly two arguments:
                     - table name
                     - destination file
+                and one optional:
+                    - batch size
                 ''')
         table_name = args[0]
         dest_file = args[1]
+        batch_size = self.DEFAULT_BATCH_SIZE if len(args) == 2 else int(args[2])
         allowed_tables = [
             'geography_ab_group',
             'geography_ab_value',
@@ -38,11 +44,20 @@ class Command(BaseCommand):
             'user_id': 'user',
             'value_id': 'value'}
         if table_name in allowed_tables:
-            cursor = connection.cursor()
-            cursor.execute('SELECT * FROM ' + table_name)
-            dump_cursor(
-                cursor,
-                dest_file,
-                **field_mapping)
+            count = 0
+            with closing(connection.cursor()) as cursor:
+                cursor.execute('SELECT COUNT(*) FROM ' + table_name + ' ORDER BY id')
+                count, = cursor.fetchone()
+
+            sql = 'SELECT * FROM ' + table_name
+            for offset in xrange(0, count, batch_size):
+                with closing(connection.cursor()) as cursor:
+                    cursor = connection.cursor()
+                    cursor.execute(sql + ' LIMIT ' + str(batch_size) + ' OFFSET ' + str(offset))
+                    dump_cursor(
+                        cursor,
+                        dest_file,
+                        append=(offset > 0),
+                        **field_mapping)
         else:
             raise CommandError('table ' + table_name + ' is not supported')
--- main/geography/utils/db.py
+++ main/geography/utils/db.py
@@ -2,13 +2,12 @@
 import csv
 
 
-def dump_cursor(cursor, dest_file, **field_mapping):
+def dump_cursor(cursor, dest_file, append=False, **field_mapping):
     headers = [field_mapping.get(col[0], col[0]) for col in cursor.description]
-    with open(dest_file, 'w') as csvfile:
+    with open(dest_file, 'a' if append else 'w') as csvfile:
         writer = csv.writer(csvfile)
         writer.writerow(headers)
-        row = cursor.fetchone()
-        while row:
+        for row in cursor:
             row = [val.encode('utf-8') if isinstance(val, unicode) else val for val in row]
             writer.writerow(row)
             row = cursor.fetchone()
