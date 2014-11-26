a0be865286fc9b424891a23123744307a4962fb0
Vit Stanislav <slaweet@seznam.cz>
2014-11-26 11:10:25 +0100

    Create templates for feedback email

--- main/proso_feedback/views.py
+++ main/proso_feedback/views.py
@@ -2,8 +2,9 @@
 from django.utils.translation import ugettext as _
 from django.utils import simplejson
 from geography.utils import JsonResponse
+from django.template.loader import render_to_string
+from django.core.mail import EmailMultiAlternatives
 from logging import getLogger
-from django.core.mail import send_mail
 from django.conf import settings
 
 
@@ -17,24 +18,28 @@ def is_likely_worthless(feedback):
 def feedback(request):
     if request.body:
         feedback = simplejson.loads(request.body)
-        mail_text = ('## This email is sent from the feedback form on the site. ##\n\n' +
-                     feedback['text'] + '\n' +
-                     '\n\n## end of user message ##\n' +
-                     '\nemail: ' + feedback['email'] +
-                     '\nuser_id: ' + str(request.user.id) +
-                     '\nusername: ' + request.user.username +
-                     '\npage: ' + feedback['page'] +
-                     '\nuser agent: ' + feedback['user_agent'])
         if is_likely_worthless(feedback):
             mail_from = settings.FEEDBACK_FROM_SPAM
         else:
             mail_from = settings.FEEDBACK_FROM
-        send_mail('slepemapy.cz feedback',
-                  mail_text,
-                  mail_from,
-                  [settings.FEEDBACK_TO],
-                  fail_silently=False)
-        LOGGER.debug("email sent %s\n", mail_text)
+
+        text_content = render_to_string("emails/feedback.plain.txt", {
+            "feedback": feedback,
+            "user": request.user,
+        })
+        html_content = render_to_string("emails/feedback.html", {
+            "feedback": feedback,
+            "user": request.user,
+        })
+        mail = EmailMultiAlternatives(
+            'slepemapy.cz feedback',
+            text_content,
+            mail_from,
+            [settings.FEEDBACK_TO],
+        )
+        mail.attach_alternative(html_content, "text/html")
+        mail.send()
+        LOGGER.debug("email sent %s\n", text_content)
         response = {
             'type': 'success',
             'msg': _('Feedback confirmed'),
