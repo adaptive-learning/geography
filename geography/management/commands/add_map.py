6174fd1b87fdda505fcf38c9b036f32a471e497c
Vit Stanislav <slaweet@seznam.cz>
2016-01-16 20:38:40 +0100

    Improve add_map command
    allow adding all maps at once

--- geography/management/commands/add_map.py
+++ geography/management/commands/add_map.py
@@ -9,6 +9,7 @@ import csv
 
 
 class Command(BaseCommand):
+    MAPS_DIR = 'geography/static/map'
     option_list = BaseCommand.option_list + (
         make_option(
             '--output',
@@ -18,20 +19,19 @@ class Command(BaseCommand):
     )
 
     def handle(self, *args, **options):
-        if len(args) < 2:
+        if len(args) < 1:
             raise CommandError(
                 "Not enough arguments. Two arguments required: " +
-                " <flashcards-file> <map-code>")
-        with open(args[0], 'r') as json_file:
+                " <flashcards-file> [<map-code>]")
+        map_code = None
+        if len(args) == 2:
             map_code = args[1]
+        with open(args[0], 'r') as json_file:
             data = json.load(json_file, 'utf-8')
 
         contexts_by_id = {}
         for c in data['contexts']:
             contexts_by_id[c['id']] = c
-        if map_code not in contexts_by_id:
-            print "missing map: %s" % map_code
-
         terms_by_id = {}
         for t in data['terms']:
             terms_by_id[t['id']] = t
@@ -40,7 +40,13 @@ class Command(BaseCommand):
         for t in data['terms']:
             categories_by_id[t['id']] = t
 
-        self.add_map(map_code, data, terms_by_id)
+        if map_code is None:
+            self.update_all_maps(data, terms_by_id)
+        else:
+            self.add_map(map_code, data, terms_by_id)
+            if map_code not in contexts_by_id:
+                print "missing map: %s" % map_code
+
         self.update_translations(data, terms_by_id, categories_by_id)
         # self.generate_translations_file(data)
 
@@ -48,6 +54,12 @@ class Command(BaseCommand):
             json.dump(data, f, indent=2)
             print ('Updated flashcards written to file: \'%s\'' %
                    options['output'])
+
+    def update_all_maps(self, data, terms_by_id):
+        for f in sorted(os.listdir(os.path.join(settings.BASE_DIR, self.MAPS_DIR))):
+            if f.endswith('.svg'):
+                print 'updating map: ' + f
+                self.add_map(f[:-4], data, terms_by_id)
 
     def generate_translations_file(self, data):
         for lang in settings.LANGUAGES:
@@ -77,33 +89,33 @@ class Command(BaseCommand):
         for f in data['flashcards']:
             flashcards_by_id[f['id']] = f
 
-        file_name = os.path.join(settings.BASE_DIR, 'geography/static/map/{0}.svg'.format(map_code))
+        file_name = os.path.join(settings.BASE_DIR, self.MAPS_DIR, '{0}.svg'.format(map_code))
         map_dom = minidom.parse(file_name)
         groups = map_dom.getElementsByTagName('g')
         for g in groups:
             paths = g.getElementsByTagName('path') + g.getElementsByTagName('circle')
             group_id = g.attributes['id'].value
             if group_id != 'bg':
-                # place_type = Place.PLACE_TYPE_SLUGS[group_id.upper()]
-                print ('## ' + group_id + ':')
+                # print ('## ' + group_id + ':')
                 for path in paths:
                     code = unicode(path.attributes['data-code'].value).encode("utf-8")
                     name = unicode(path.attributes['data-name'].value).encode("utf-8")
                     if code not in terms_by_id:
-                        term = {
-                            'id': code,
-                            'name-cs': name,
-                            'type': group_id,
-                            'categories': [map_code],
-                        }
-                        data['terms'].append(term)
-                        flashcard = {
-                            'id': map_code + '-' + code,
-                            'term': code,
-                            'context': map_code,
-                            'description': code,
-                        }
-                        data['flashcards'].append(flashcard)
-                        print 'Flashcard added: ' + name
+                        if code != '':
+                            term = {
+                                'id': code,
+                                'name-cs': name,
+                                'type': group_id,
+                                'categories': [map_code],
+                            }
+                            data['terms'].append(term)
+                            flashcard = {
+                                'id': map_code + '-' + code,
+                                'term': code,
+                                'context': map_code,
+                                'description': code,
+                            }
+                            data['flashcards'].append(flashcard)
+                            print 'Flashcard added: ' + name + ' ' + code
                     elif map_code not in terms_by_id[code]['categories']:
                         terms_by_id[code]['categories'].append(map_code)
