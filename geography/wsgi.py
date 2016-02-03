72c7fbaf203c52242858e219bce171ac83275e85
Jan Papousek <jan.papousek@gmail.com>
2016-02-03 10:52:05 +0100

    remove obsolete docs

--- geography/wsgi.py
+++ geography/wsgi.py
@@ -1,12 +1,3 @@
-"""
-WSGI config for geography project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
-"""
-
 import os
 import sys
 DIRNAME = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
