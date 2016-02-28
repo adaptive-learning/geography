32e7ad7172af834936369b900ef3b5e040fae23c
Vit Stanislav <slaweet@seznam.cz>
2016-02-28 20:00:11 +0100

    Set up options selection A/B experiment

---
+++ geography/option_selection.py
@@ -0,0 +1,9 @@
+from proso.models.option_selection import OptionsNumber
+
+
+class PartialyFourOptionsNumber(OptionsNumber):
+    def compute_number_of_options(self, target_probability, prediction):
+        if prediction >= target_probability:
+            return 0
+        else:
+            return 4
